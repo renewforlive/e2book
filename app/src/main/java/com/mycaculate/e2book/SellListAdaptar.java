4c4952dfcd895c67cc9a17ceba3cdd8c13546c30
renewforlive <renewforlive1@gmail.com>
2018-07-27 11:00:01 +0800

    20180727

--- app/src/main/java/com/mycaculate/e2book/SellListAdaptar.java
+++ app/src/main/java/com/mycaculate/e2book/SellListAdaptar.java
@@ -115,8 +115,13 @@ public class SellListAdaptar extends BaseAdapter{
 
                 builder = new AlertDialog.Builder(context);
                 builder.setTitle("對這本書感興趣的人")
-                        .setView(view);
-
+                        .setView(view)
+                        .setPositiveButton("關閉", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                alertDialog.dismiss();
+                            }
+                        });
                 alertDialog = builder.create();
                 alertDialog.show();
             }
